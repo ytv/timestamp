385bab08c4cc0cf3fb048899d81c7ed2e6511f7f
ytv <sillying@gmail.com>
2016-06-07 22:24:53 -0700

    fixed tools module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var tools = require('./scripts/helper/dateTools');
+var path = require('path');
 var express = require('express');
 var app = express();
-var path = require('path');
+
 var port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname + '/public')));
